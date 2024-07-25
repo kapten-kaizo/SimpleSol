507b1e3eb027e9a050e5beb09f7a02fd868a5718
KY ^ <73789857+kapten-kaizo@users.noreply.github.com>
2024-07-25 13:33:23 +0700

    Update SimpleSol.sol

--- SimpleSol.sol
+++ SimpleSol.sol
@@ -1,9 +1,10 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
 contract simpleScript {
-	uint256 private number;
-	
-	function addLiquidityETH(uint256 num) external {
-		number += num;
-	}
+        uint256 private number;
+
+        function addLiquidityETH(uint256 num) external {
+                number += num;
+        }
 }
